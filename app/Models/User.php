b61273001e9b0b5fbd372b594c5c0e0adb088c5f
rmrrwnnr <ivanova_viktoria2611@mail.ru>
2026-03-24 00:10:05 +0500

    Установка Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable 
 {
+    use HasApiTokens;
     use HasFactory, Notifiable;
 
     protected $fillable = [
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\CategoryControllerApi;
 use App\Http\Controllers\ServiceControllerApi;
 use App\Http\Controllers\AppointmentControllerApi;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/category', [CategoryControllerApi::class, 'index']);
 Route::get('/category/{id}', [CategoryControllerApi::class, 'show']);
@@ -13,3 +17,19 @@ Route::get('/service/{id}', [ServiceControllerApi::class, 'show']);
 
 Route::get('/appointments', [AppointmentControllerApi::class, 'index']);
 Route::get('/appointments/{id}', [AppointmentControllerApi::class, 'show']);
+
+// Route::middleware('auth:sanctum')->get('/appointments', [AppointmentControllerApi::class, 'index']);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::get('/appointments', [AppointmentControllerApi::class, 'index']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
